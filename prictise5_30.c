70c0303b27241f471c6b4c15f310041bb8dfe097
KEYloyee <yirenxingzhe@Outlook.com>
2026-06-05 10:23:11 +0800

    Add files via upload
    判断hyw是不是0

---
+++ prictise5_30.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{   
+    printf("现在判断胡乙未是不是0\n");
+    printf("请输入一个数\n");
+    int a;
+    scanf("%d",&a);
+    if (a==0)
+        printf("胡乙未是0\n");
+    else
+        printf("胡乙未不是0\n");
+    return 0;  
+}
